e2d61c813e69d504439145b0f08b3e7c22c92796
Lian <913240512@qq.com>
2021-11-23 10:11:23 +0800

    init project

---
+++ src/main/java/com/example/kehou/KehouApplication.java
@@ -0,0 +1,13 @@
+package com.example.kehou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KehouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KehouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kehou/KehouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kehou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KehouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
